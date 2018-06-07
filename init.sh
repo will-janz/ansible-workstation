5aa4fd0f5885b5e38f8f560170375d683f61b95a
Will J <willj.94@gmail.com>
2018-06-07 12:29:57 -0600

    fix ~ not expanding in quotes I think

--- init.sh
+++ init.sh
@@ -4,16 +4,16 @@
 # Not using Ansible to generate the ssh key because I want to use a passphrase
 # and not have the passphrase in git in any way.
 
-if [ ! -d "~/.ssh" ]; then
-  mkdir ~/.ssh
+if [ ! -d "/home/will/.ssh" ]; then
+  mkdir /home/will/.ssh
 fi
 
-EDKEYFILE="~/.ssh/id_ed25519_$(date +'%Y')"
+EDKEYFILE="/home/will/.ssh/id_ed25519_$(date +'%Y')"
 if [ ! -f $EDKEYFILE ]; then
   ssh-keygen -o -a 100 -t ed25519 -f $EDKEYFILE
 fi
 
-RSAKEYFILE="~/.ssh/id_rsa_$(date +'%Y')"
+RSAKEYFILE="/home/will/.ssh/id_rsa_$(date +'%Y')"
 if [ ! -f $RSAKEYFILE ]; then
   ssh-keygen -o -a 100 -t rsa -b 4096 -f $RSAKEYFILE
 fi
